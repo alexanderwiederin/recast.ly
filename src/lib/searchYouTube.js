5cecdabe03beebd1daa24299e8dbccda89557854
alexanderwiederin <alexander.wiederin@gmail.com>
2018-10-01 22:46:15 -0700

    [Mostly Completed] Bare Minimum Requirements

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,25 @@
 var searchYouTube = (options, callback) => {
-  // TODO
+  
+  $.ajax({
+    url: 'https://www.googleapis.com/youtube/v3/search',
+    type: 'GET',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      type: 'video'
+    },
+    
+    success: (data) => callback(data.items),
+    
+    error: () => console.log('Failed to get data!')
+    
+  });
 };
 
 export default searchYouTube;
+
+
+
+// GET https://www.googleapis.com/youtube/v3/search
